ef4711fedd1b01283034003d7ba58265b05906bd
RaFat <manrafat2006@gmail.com>
2025-05-24 17:52:30 +0700

    Update categories

--- EX-2/controllers/categoriesController.js
+++ EX-2/controllers/categoriesController.js
@@ -1,37 +1,39 @@
 import { categories } from "../models/data.js";
 
-export const getAllCategories= (req, res)=>{
+export const getAllCategories = (req, res) => {
     res.json(categories);
-}
-export const getCategoriesById= (req, res)=>{
+};
+
+export const getCategoriesById = (req, res) => {
     const categoryId = parseInt(req.params.id);
-    const category= categories.find(c=> c.id === categoryId);
-    if(!category) return res.status(404).json({error: 'Categories not found'});
+    const category = categories.find(c => c.id === categoryId);
+    if (!category) return res.status(404).json({ error: 'Category not found' });
     res.json(category);
-}
-export const createNewCategories= (req, res)=>{
-    const {name, email} = req.body;
-    if(!name || !email){
-        return res.status(400).json({error: 'Name and email are required!!'});
+};
+
+export const createNewCategories = (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({ error: 'Name is required!' });
     }
     const newCategory = {
-        id: categories.length+1,
-        name,
-        email
+        id: categories.length + 1,
+        name
     };
     categories.push(newCategory);
     res.status(201).json(newCategory);
-}
-export const updateCategories= (req, res)=>{
+};
+
+export const updateCategories = (req, res) => {
     const categoryId = parseInt(req.params.id);
-    const category= categories.find(c=> c.id === categoryId);
-    const {name, email}= req.body;
-    if(!category) return res.status(404).json({error: 'Categories not found'});
-    if(name) category.name= name;
-    if(email) category.email= email;
+    const category = categories.find(c => c.id === categoryId);
+    const { name } = req.body;
+    if (!category) return res.status(404).json({ error: 'Category not found' });
+    if (name) category.name = name;
 
     res.json(category);
-}
+};
+
 export const deleteCategories = (req, res) => {
     const categoryId = parseInt(req.params.id);
     const index = categories.findIndex(c => c.id === categoryId);
